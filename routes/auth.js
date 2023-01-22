dad47221c5082c86588067d00370f090f965d460
Joeyyy09 <imt_2021063@iiitm.ac.in>
2023-01-22 14:17:10 +0530

    Login feature added, but somethiing fishy

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.post("/signup",body("email").isEmail().withMessage("Enter a valid email"),body("password").isLength({min : 6}).withMessage("Enter a password with minimun of 6 charecters"),signup)
 router.post("/refresh-token",)
+router.post('/signin',body("email").isEmail().withMessage("Enter a valid email"),body("password").isLength({min : 6}).withMessage("Enter a password with minimun of 6 charecters"), signin )
 router.get("/signin",signin)
 
 module.exports = router
