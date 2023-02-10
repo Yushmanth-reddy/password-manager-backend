988d74d57627cd6b55bbccc4c84645aa4c477480
yushmanth <yushmanth@yushmanth-pc.com>
2023-02-10 19:19:24 +0530

    password checking rectified

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,150 +1,144 @@
 const User = require("../models/user");
-const {body,validationResult} = require('express-validator')
-const bcrypt = require("bcrypt")
-const jwt = require('jsonwebtoken');
-const client = require('../configs/redis');
-const NodeRSA = require('node-rsa');
-const key = new NodeRSA({b: 1024});
+const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const client = require("../configs/redis");
+const NodeRSA = require("node-rsa");
+const key = new NodeRSA({ b: 1024 });
 
 const accessTokenGenerator = (user) => {
-    const payload = {}
-    const options = {
-        expiresIn : "1h",
-        audience: [user._id]
-    }
-    var accessToken = jwt.sign(payload, process.env.JWT_ACCESS_KEY,options);
+  const payload = {};
+  const options = {
+    expiresIn: "1h",
+    audience: [user._id],
+  };
+  var accessToken = jwt.sign(payload, process.env.JWT_ACCESS_KEY, options);
 
-    return accessToken
-}
+  return accessToken;
+};
 
 const refreshTokenGenerator = (user) => {
-    const payload = {}
-    const options = {
-        expiresIn : "1y",
-        audience: [user._id]
-    }
-    var refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_KEY,options);
-    const userId = user._id.toString()
+  const payload = {};
+  const options = {
+    expiresIn: "1y",
+    audience: [user._id],
+  };
+  var refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_KEY, options);
+  const userId = user._id.toString();
 
-    client.SET(userId,refreshToken, {EX: 365 * 24 * 60 * 60  } ,(err)=>{
+  client.SET(userId, refreshToken, { EX: 365 * 24 * 60 * 60 }, (err) => {
+    if (err) {
+      res.status(500).json({
+        message: "internal server error",
+      });
+    }
+  });
+
+  return refreshToken;
+};
+
+exports.signup = async (req, res) => {
+  const { email, name, password } = req.body;
+
+  const validationErrors = validationResult(req);
+  if (validationErrors.isEmpty()) {
+    const isExist = await User.findOne({ email: email });
+
+    if (isExist) {
+      res.json({
+        message: "user already exists",
+      });
+    } else {
+      bcrypt.hash(password, 10, async (err, hash) => {
         if (err) {
-            res.status(500).json({
-                message:"internal server error"
-            })
-        } 
-    })
-    
-    return refreshToken
-}
+          res.json({
+            msg: "err in hashing password",
+          });
+        } else {
+          const publicKey = key.exportKey("public");
+          const privateKey = key.exportKey("private");
+          const user = new User({
+            email,
+            name,
+            password: hash,
+            publicKey,
+          });
 
+          await user.save();
+          const accessToken = accessTokenGenerator(user);
+          const refreshToken = refreshTokenGenerator(user);
 
-
-exports.signup = async(req,res) => {
-    const {email,name,password} = req.body;
-
-    const validationErrors = validationResult(req)
-    if(validationErrors.isEmpty()){
-        const isExist = await User.findOne({email:email});
-
-        if(isExist){
-            res.json({
-                message:"user already exists"
-            })
+          res.status(200).json({ accessToken, refreshToken, privateKey });
         }
-        else{
-            bcrypt.hash(password, 10, async (err, hash) => {
-                if (err) {
-                    res.json({
-                        msg:"err in hashing password"
-                    })
-                } 
-                else {
-                    const publicKey = key.exportKey('public');
-                    const privateKey = key.exportKey('private');
-                    const user = new User({
-                        email,
-                        name,
-                        password:hash,
-                        publicKey
-                    })
-                    
-                    await user.save();
-                    const accessToken = accessTokenGenerator(user);
-                    const refreshToken = refreshTokenGenerator(user);
-    
-                    res.status(200).json({accessToken,refreshToken,privateKey})
-                }
-                
-            });
+      });
+    }
+  } else {
+    const errors = validationErrors.array().map((err) => {
+      return {
+        msg: err.msg,
+      };
+    });
+    res.json({
+      errors,
+    });
+  }
+};
+
+exports.signin = async (req, res) => {
+  const { email, password } = req.body;
+  const validationErrors = validationResult(req);
+  if (validationErrors.isEmpty()) {
+    const isExist = await User.findOne({ email: email });
+    if (!isExist) {
+      res.json({ message: "User does not exist" });
+    }
+    //if user exist than compare password
+    //password comes from the user
+    //user.password comes from the database
+    else {
+      await bcrypt.compare(password, isExist.password, (err, data) => {
+        if (err) {
+          res.json({
+            msg: "err in hashing password",
+          });
+        } else if (!data) {
+          res.json({ msg: "Password Incorrect" });
+        } else {
+          const accessToken = accessTokenGenerator(isExist);
+          const refreshToken = refreshTokenGenerator(isExist);
+          res.json({ accessToken, refreshToken });
         }
-
+      });
     }
-    else{
-        const errors = validationErrors.array().map((err) => {
-            return{
-                msg:err.msg
-            }
-        })
-        res.json({
-            errors
-        })
-    }
+  } else {
+    res.json({
+      msg: validationErrors,
+    });
+  }
+};
 
-}
+exports.refreshToken = async (req, res) => {
+  const user = req.user;
 
-exports.signin = async (req,res) => {
-    const {email,password} = req.body;
-    const validationErrors = validationResult(req);
-    if(validationErrors.isEmpty()){
-        const isExist = await User.findOne({email:email});
-            if (!isExist) {
-                res.json({ message: "User does not exist" 
-            })
-        }
-        //if user exist than compare password
-            //password comes from the user
-            //user.password comes from the database
-            else{
-                bcrypt.compare(password, isExist.password, (err, data) => {
-                    if(err){
-                        res.json({
-                            msg:"err in hashing password"
-                        })
-                    }
-                    else{
-                        const accessToken = accessTokenGenerator(isExist);
-                        const refreshToken = refreshTokenGenerator(isExist);
-                        res.json({accessToken,refreshToken})
-                    }
-                  }
-                )
-            }
-           
-    }
-}
+  const accessToken = accessTokenGenerator(user);
+  const refreshToken = refreshTokenGenerator(user);
 
-exports.refreshToken = async (req,res) => {
-    const user = req.user;
+  res.json({ accessToken, refreshToken });
+};
 
-    const accessToken = accessTokenGenerator(user);
-    const refreshToken = refreshTokenGenerator(user);
-
-    res.json({accessToken,refreshToken})
-}
-
-exports.logout = async (req,res) => {
-    const userId = req.user._id.toString()
-    client.DEL(userId)
-    .then((data)=>{
-        res.json({
-            msg:"User loggedout successfully"
-        })
+exports.logout = async (req, res) => {
+  const userId = req.user._id.toString();
+  client
+    .DEL(userId)
+    .then((data) => {
+      res.json({
+        msg: "User loggedout successfully",
+      });
     })
-    .catch((err)=>{
-        console.log(err);
-        res.status(500).json({
-            msg:"internal server error"
-        })
-    })
-}
-
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        msg: "internal server error",
+      });
+    });
+};
--- controllers/password.js
+++ controllers/password.js
@@ -1,98 +1,101 @@
-const NodeRSA = require('node-rsa');
-const key = new NodeRSA({b: 1024});
-const client = require('../configs/redis');
-const Password = require('../models/passwords')
+const NodeRSA = require("node-rsa");
+const key = new NodeRSA({ b: 1024 });
+const client = require("../configs/redis");
+const Password = require("../models/passwords");
 
-exports.addPass = async (req,res) => {
-    const {websiteURL, password} = req.body;
-    const user = req.user;
+exports.addPass = async (req, res) => {
+  const { websiteURL, password } = req.body;
+  const user = req.user;
 
-    const publicKey = req.user.publicKey;
-    const key_public = new NodeRSA(publicKey)
+  const publicKey = req.user.publicKey;
+  const key_public = new NodeRSA(publicKey);
 
-    const encryptedPassword = key_public.encrypt(password,'base64');
+  const encryptedPassword = key_public.encrypt(password, "base64");
 
-    const passwords = new Password({
-        email:user.email,
-        password:encryptedPassword,
-        websiteURL
+  const passwords = new Password({
+    email: user.email,
+    password: encryptedPassword,
+    websiteURL,
+  });
+
+  await passwords
+    .save()
+    .then(() => {
+      res.status(200).json({
+        msg: "password stored",
+      });
     })
+    .catch((err) => {
+      res.status(500).json({
+        msg: "internal server error",
+      });
+    });
+};
 
-    await passwords.save()
-    .then(()=>{
-        res.status(200).json({
-            msg:"password stored"
-        })
-    })
-    .catch((err)=>{
-        res.status(500).json({
-            msg:"internal server error"
-        })
-    })
-}
+exports.showPass = async (req, res) => {
+  const passId = req.params.passId;
+  const user = req.user;
 
-exports.showPass = async (req,res) =>{
-    const passId = req.params.passId;
-    const user = req.user;
+  const publicKey = req.user.publicKey;
+  const privateKey = await client.GET(publicKey);
 
-    const publicKey = req.user.publicKey;
-    const privateKey = await client.GET(publicKey);
-    
-    // const key_public = new NodeRSA(publicKey)
-    const key_private = new NodeRSA(privateKey)
+  // const key_public = new NodeRSA(publicKey)
+  const key_private = new NodeRSA(privateKey);
 
-    
-    await Password.findOne({_id:passId})
-    .then((pass)=>{
-        const decryptedPassword = key_private.decrypt(pass.password,'utf8')
-        res.status(200).json({
-            websiteURL : pass.websiteURL,
-            password : decryptedPassword
-        })
+  await Password.findOne({ _id: passId })
+    .then((pass) => {
+      const decryptedPassword = key_private.decrypt(pass.password, "utf8");
+      res.status(200).json({
+        websiteURL: pass.websiteURL,
+        password: decryptedPassword,
+      });
     })
-    .catch((err)=>{
-        res.status(400).json({
-            msg:"Password not found",
-            mse:err.msg
-        })
-    })
-}
+    .catch((err) => {
+      res.status(400).json({
+        msg: "Password not found",
+        mse: err.msg,
+      });
+    });
+};
 
-exports.deletePass = async (req,res) => {
-    const passId = req.params.passId;
-    const user = req.user;
-    
-    await Password.deleteOne({_id:passId})
-    .then(()=>{
-        res.status(200).json({
-            msg:"Password deleted"
-        })
-    })
-    .catch((err)=>{
-        res.json({
-            msg:"password not found"
-        })
-    })
-}
+exports.deletePass = async (req, res) => {
+  const passId = req.params.passId;
+  const user = req.user;
 
-exports.updatePass = async (req,res) => {
-    const passId = req.params.passId;
-    const user = req.user;
-    const {websiteURL, password} = req.body;
-    
-    const publicKey = req.user.publicKey;
-    const key_public = new NodeRSA(publicKey)
+  await Password.deleteOne({ _id: passId })
+    .then(() => {
+      res.status(200).json({
+        msg: "Password deleted",
+      });
+    })
+    .catch((err) => {
+      res.json({
+        msg: "password not found",
+      });
+    });
+};
 
-    const encryptedPassword = key_public.encrypt(password,'base64');
-    await Password.updateOne({_id:passId},{$set:{password:encryptedPassword,websiteURL}})
-    .then(()=>{
-        res.status(200).json({
-            msg:"Password updated"
-        })
+exports.updatePass = async (req, res) => {
+  const passId = req.params.passId;
+  const user = req.user;
+  const { websiteURL, password } = req.body;
+
+  const publicKey = req.user.publicKey;
+  const key_public = new NodeRSA(publicKey);
+
+  const encryptedPassword = key_public.encrypt(password, "base64");
+  await Password.updateOne(
+    { _id: passId },
+    { $set: { password: encryptedPassword, websiteURL } }
+  )
+    .then(() => {
+      res.status(200).json({
+        msg: "Password updated",
+      });
     })
-    .catch((err)=>{
-        res.json({
-            msg:"Error in updating password"
-        })
-    })
-}
+    .catch((err) => {
+      res.json({
+        msg: "Error in updating password",
+      });
+    });
+};
