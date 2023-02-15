bb3366f040cba42e0ff295f4464b9f7144ba00d9
AvijeetJain <jainavijeet@gmail.com>
2023-02-15 19:37:51 +0530

    added get all pass route

--- routes/password.js
+++ routes/password.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { addPass, showPass, deletePass, updatePass } = require("../controllers/password");
+const { addPass, showPass, deletePass, updatePass, getallpass } = require("../controllers/password");
 const { verifyRefresh, verifyAccess } = require("../middleware/verifyToken");
 const router = express.Router();
 
@@ -7,5 +7,7 @@ router.post("/addPass",verifyAccess,addPass);
 router.get("/showPass/:passId",verifyAccess,showPass)
 router.delete("/delPass/:passId",verifyAccess,deletePass)
 router.put("/updatePass/:passId",verifyAccess,updatePass)
+
+router.get("/getallpass", verifyAccess, getallpass);
 
 module.exports = router;
