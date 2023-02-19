c9d09af7dfb01a888a443f1812d3a5880a933f1a
yushmanth <yushmanth@yushmanth-pc.com>
2023-02-19 23:14:21 +0530

    solve private key new line issue

--- controllers/Pkey.js
+++ controllers/Pkey.js
@@ -1,13 +1,17 @@
 const client = require("../configs/redis");
 
 exports.storePrivateKey = async (req, res) => {
-  const { privateKey } = req.body;
+  let { privateKey } = req.body;
   const publicKey = req.user.publicKey;
   const data = await client.SET(publicKey, privateKey, { EX: 600 });
+  privateKey = privateKey.replace(/\\n/g, "\n");
   if (data === "OK") {
     res.status(200).json({
       msg: "Private key stored",
     });
+    // console.log(typeof privateKey);
+    console.log(privateKey);
+    // console.log("hello\nyushman");
   } else {
     res.status(500).json({
       message: "internal server error",
